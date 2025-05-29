2312a61141478045680808ca08b7c7e493e7eae6
Minh Tri <nguyenminhtri1808t@gmail.com>
2025-05-29 19:35:57 +0700

    feat(types/responses.type.ts): add new type ErrorResponse

--- src/types/responses.type.ts
+++ src/types/responses.type.ts
@@ -1,7 +1,27 @@
+import { ErrorInstance } from '../constants/errorInstance.constant.js';
+import { ErrorCodes } from '../constants/errorCodes.constants.js';
+
 export type SuccessResponseType<T = null, U = null> = {
   success?: true;
   statusCode: number;
   message: string;
   data?: T | null;
   metadata?: U | null;
+};
+
+export type ErrorResponseType<T = null> = {
+  success?: boolean;
+  statusCode: number;
+  message: string;
+  error: {
+    code: ErrorCodes;
+    message: string;
+    name: string;
+    details?: T[] | null;
+    instance: ErrorInstance;
+    devInfo?: {
+      stack: string;
+      isOperational: boolean;
+    };
+  };
 };
