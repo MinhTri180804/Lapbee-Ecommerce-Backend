8c80f3a1f7eb80f65ef44caee3aa21511d376428
Minh Tri <nguyenminhtri1808t@gmail.com>
2025-06-04 20:51:45 +0700

    fix(apperror.error.ts): convert AppError to abstract class

--- src/errors/AppError.error.ts
+++ src/errors/AppError.error.ts
@@ -1,18 +1,32 @@
 import { StatusCodes } from 'http-status-codes';
-import { ErrorInstance } from '../constants/errorInstance.constant.js';
+import { ErrorCodes, ErrorCodesValues } from '../constants/errorCodes.constant.js';
+import { ErrorInstance, ErrorInstanceKeys, ErrorInstanceValues } from '../constants/errorInstance.constant.js';
+import { ErrorMessages } from '../constants/errorMessages.constant.js';
 
-export class AppError extends Error {
+type AppErrorConstructor<T> = {
+  statusCode: StatusCodes;
+  errorInstanceKey: ErrorInstanceKeys;
+  details: T;
+  message?: string | null;
+};
+
+export class AppError<T> extends Error {
   readonly statusCode: StatusCodes;
-  readonly isOperational: boolean = true;
-  readonly errorInstance: ErrorInstance = ErrorInstance.APP;
+  readonly isOperational: boolean;
+  readonly errorInstance: ErrorInstanceValues;
+  readonly errorCode: ErrorCodesValues;
+  readonly name: ErrorInstanceValues | string;
+  readonly details: T | null;
 
-  static readonly staticErrorInstance: ErrorInstance = ErrorInstance.APP;
-
-  constructor(statusCode: StatusCodes, message: string) {
-    super(message);
+  protected constructor({ statusCode, errorInstanceKey, details, message }: AppErrorConstructor<T>) {
+    super(message || ErrorMessages[ErrorInstance[errorInstanceKey]]);
     this.statusCode = statusCode;
-    this.name = 'AppError';
-
+    this.errorInstance = ErrorInstance[errorInstanceKey];
+    this.errorCode = ErrorCodes[this.errorInstance];
+    this.details = details;
+    this.isOperational = errorInstanceKey === 'UNKNOWN' ? false : true;
+    this.name = ErrorInstance[errorInstanceKey];
+    this.message = message || ErrorMessages[this.errorInstance];
     Object.setPrototypeOf(this, new.target.prototype);
 
     Error.captureStackTrace(this, this.constructor);
