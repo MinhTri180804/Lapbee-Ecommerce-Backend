7c1e17f8fcd5b71e80f104057f8a0bf7099c5d83
Minh Tri <nguyenminhtri1808t@gmail.com>
2025-06-05 00:05:35 +0700

    test(__tests__): impement testing for method isMatchTypeStringValue of JWTGenerator

--- src/__tests__/jwtGenerator.util.test.ts
+++ src/__tests__/jwtGenerator.util.test.ts
@@ -38,3 +38,33 @@ describe('JWTGenerator - setPassword', () => {
     expect(token).toBe('mocked.jwt.token');
   });
 });
+
+describe('JWTGenerator method isMatchWithTypeStringValue', () => {
+  it('It Not Match', () => {
+    const value = '5';
+    const result = JWTGenerator.isMatchWithTypeStringValue(value);
+
+    expect(result).toBe(false);
+  });
+
+  it('It Not Match', () => {
+    const value = '7x';
+    const result = JWTGenerator.isMatchWithTypeStringValue(value);
+
+    expect(result).toBe(false);
+  });
+
+  it('It Match', () => {
+    const value = '7d';
+    const result = JWTGenerator.isMatchWithTypeStringValue(value);
+
+    expect(result).toBe(true);
+  });
+
+  it('It Match', () => {
+    const value = '7 d';
+    const result = JWTGenerator.isMatchWithTypeStringValue(value);
+
+    expect(result).toBe(true);
+  });
+});
