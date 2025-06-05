d5c3d2c610268cb3b79f4001bb1feb42adab994d
Minh Tri <nguyenminhtri1808t@gmail.com>
2025-06-06 04:06:37 +0700

    feat(configs-utils): implement method accessToken and refreshToken in JWTGenerator
    Implement utility methods in JWTGenerator to generate accessToken and refreshToken for
    authentication purposes.
    
    Closes #103

--- src/utils/JwtGenerator.util.ts
+++ src/utils/JwtGenerator.util.ts
@@ -1,26 +1,54 @@
 import { env } from '../configs/env.config.js';
 import jwt from 'jsonwebtoken';
 import type { StringValue } from 'ms';
+import { UserAuthRoleEnum } from 'src/enums/userAuthRole.enum.js';
 import { v4 as uuidV4 } from 'uuid';
 
 type SetPasswordParams = {
   userAuthId: string;
 };
 
-class _JWTGenerator {
-  static instance: _JWTGenerator;
+type AccessTokenParams = {
+  userAuthId: string;
+  role: UserAuthRoleEnum;
+};
 
-  constructor() {}
+type RefreshTokenParams = {
+  userAuthId: string;
+};
 
-  static getInstance() {
-    if (!_JWTGenerator.instance) {
-      _JWTGenerator.instance = new _JWTGenerator();
-    }
-
-    return _JWTGenerator.instance;
+export class JWTGenerator {
+  static accessToken({ userAuthId, role }: AccessTokenParams): string {
+    const jti = uuidV4();
+    const SECRET_KEY = env.jwt.SECRET_KEY.ACCESS_TOKEN as string;
+    const EXPIRES = env.expiredTime.hours.ACCESS_TOKEN as string;
+    const accessToken = jwt.sign(
+      {
+        role: role
+      },
+      SECRET_KEY,
+      {
+        subject: userAuthId,
+        expiresIn: EXPIRES as StringValue,
+        jwtid: jti
+      }
+    );
+    return accessToken;
   }
 
-  public setPassword({ userAuthId }: SetPasswordParams) {
+  static refreshToken({ userAuthId }: RefreshTokenParams): string {
+    const jti = uuidV4();
+    const SECRET_KEY = env.jwt.SECRET_KEY.REFRESH_TOKEN as string;
+    const EXPIRES = env.expiredTime.day.REFRESH_TOKEN as string;
+    const refreshToken = jwt.sign({}, SECRET_KEY, {
+      subject: userAuthId,
+      expiresIn: EXPIRES as StringValue,
+      jwtid: jti
+    });
+    return refreshToken;
+  }
+
+  static setPassword({ userAuthId }: SetPasswordParams): string {
     const jti = uuidV4();
     const SECRET_KEY = env.jwt.SECRET_KEY.SET_PASSWORD as string;
     const EXPIRES = env.expiredTime.day.TOKEN_SET_PASSWORD as string;
@@ -33,9 +61,7 @@ class _JWTGenerator {
     return jwtToken;
   }
 
-  public isMatchWithTypeStringValue(value: string): boolean {
+  static isMatchWithTypeStringValue(value: string): boolean {
     return /^\d+\s?(d|h|m|s)$/i.test(value);
   }
 }
-
-export const JWTGenerator = _JWTGenerator.getInstance();
