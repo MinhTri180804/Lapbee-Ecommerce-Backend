23acb6d5bb1c399495194385ee6299dbc23aeec0
Minh Tri <nguyenminhtri1808t@gmail.com>
2025-06-06 07:24:12 +0700

    feat(_tests__-services-utils): implement method verifyToken in JWTGenerator
    Create a utility method to verify whether a JWT token has expired or not.
    
    Closes #96

--- src/utils/JwtGenerator.util.ts
+++ src/utils/JwtGenerator.util.ts
@@ -1,10 +1,12 @@
-import { env } from '../configs/env.config.js';
 import jwt from 'jsonwebtoken';
 import type { StringValue } from 'ms';
-import { UserAuthRoleEnum } from 'src/enums/userAuthRole.enum.js';
+import { UserAuthRoleEnum } from '../enums/userAuthRole.enum.js';
+import { JWTTokenExpiredError } from '../errors/JwtTokenExpired.error.js';
+import { JWTTokenInvalidError } from '../errors/JwtTokenInvalid.error.js';
 import { v4 as uuidV4 } from 'uuid';
+import { env } from '../configs/env.config.js';
 
-type SetPasswordParams = {
+type CreatePasswordParams = {
   userAuthId: string;
 };
 
@@ -17,7 +19,58 @@ type RefreshTokenParams = {
   userAuthId: string;
 };
 
+type VerifyTokenParams = {
+  token: string;
+  typeToken: TypeTokenKeys;
+};
+
+type TokenMessageErrorExpired = {
+  [key in TypeTokenKeys]: string;
+};
+
+type TokenMessageErrorInvalid = {
+  [key in TypeTokenKeys]: string;
+};
+
+const TypeToken = {
+  ACCESS_TOKEN: env.jwt.SECRET_KEY.ACCESS_TOKEN,
+  REFRESH_TOKEN: env.jwt.SECRET_KEY.REFRESH_TOKEN,
+  CREATE_PASSWORD_TOKEN: env.jwt.SECRET_KEY.SET_PASSWORD
+} as const;
+
+const tokenMessageErrorExpired: TokenMessageErrorExpired = {
+  ACCESS_TOKEN: 'AccessToken expired',
+  REFRESH_TOKEN: 'RefreshToken expired',
+  CREATE_PASSWORD_TOKEN: 'CreatePasswordToken expired'
+};
+
+const tokenMessageErrorInvalid: TokenMessageErrorInvalid = {
+  ACCESS_TOKEN: 'AccessToken Invalid',
+  REFRESH_TOKEN: 'RefreshToken Invalid',
+  CREATE_PASSWORD_TOKEN: 'CreatePasswordToken Invalid'
+};
+
+type TypeTokenKeys = keyof typeof TypeToken;
+
 export class JWTGenerator {
+  static verifyToken({ token, typeToken }: VerifyTokenParams): jwt.JwtPayload {
+    try {
+      const SECRET_KEY = TypeToken[typeToken] as string;
+      const decoded = jwt.verify(token, SECRET_KEY) as jwt.JwtPayload;
+      return decoded;
+    } catch (error) {
+      if (error instanceof jwt.TokenExpiredError) {
+        throw new JWTTokenExpiredError({ message: tokenMessageErrorExpired[typeToken] });
+      }
+
+      if (error instanceof jwt.JsonWebTokenError) {
+        throw new JWTTokenInvalidError({ message: tokenMessageErrorInvalid[typeToken] });
+      }
+
+      throw error;
+    }
+  }
+
   static accessToken({ userAuthId, role }: AccessTokenParams): string {
     const jti = uuidV4();
     const SECRET_KEY = env.jwt.SECRET_KEY.ACCESS_TOKEN as string;
@@ -48,7 +101,7 @@ export class JWTGenerator {
     return refreshToken;
   }
 
-  static setPassword({ userAuthId }: SetPasswordParams): string {
+  static createPassword({ userAuthId }: CreatePasswordParams): string {
     const jti = uuidV4();
     const SECRET_KEY = env.jwt.SECRET_KEY.SET_PASSWORD as string;
     const EXPIRES = env.expiredTime.day.TOKEN_SET_PASSWORD as string;
