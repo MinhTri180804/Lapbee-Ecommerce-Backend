91478898a9cb2bcfb5b40e6d96407dbff6051522
Minh Tri <nguyenminhtri1808t@gmail.com>
2025-06-08 08:57:12 +0700

    feat(constants-__tests__-errors): implement InvalidCredentialsError
    Create a custom error class InvalidCredentialsError to standardize the error response when a user
    provides incorrect email or password during authentication.
    
    Closes #130

--- src/constants/errorCodes.constant.ts
+++ src/constants/errorCodes.constant.ts
@@ -20,7 +20,8 @@ export const ErrorCodes: ErrorCodes = {
   [ErrorInstance.PIN_CODE_REQUEST_TOO_SOON]: 4029,
   [ErrorInstance.PIN_CODE_NOTFOUND]: 4010,
   [ErrorInstance.NOTFOUND_EMAIL_SET_PASSWORD_TOKEN]: 4000,
-  [ErrorInstance.ACCOUNT_LOCKED]: 4030
+  [ErrorInstance.ACCOUNT_LOCKED]: 4030,
+  [ErrorInstance.INVALID_CREDENTIALS]: 4010
 } as const;
 
 export type ErrorCodesKeys = keyof typeof ErrorCodes;
