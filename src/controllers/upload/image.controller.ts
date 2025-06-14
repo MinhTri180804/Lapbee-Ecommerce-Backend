f8bf7eacc8cbc6319e4e4a085997705c372a0b25
Minh Tri <nguyenminhtri1808t@gmail.com>
2025-06-14 12:56:21 +0700

    feat(controllers-services-routes): implement api delete image logo brand
    Implement an API endpoint to delete a previously uploaded brand logo image from the cloud storage
    (e.g., Cloudinary) using its public ID.
    
    Closes #188

--- src/controllers/upload/image.controller.ts
+++ src/controllers/upload/image.controller.ts
@@ -1,10 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
-import { UploadImageService } from '../../services/UploadImage.service';
-import { sendSuccessResponse } from '../../utils/responses.util.js';
 import { StatusCodes } from 'http-status-codes';
+import { UploadImageService } from '../../services/UploadImage.service.js';
+import { sendSuccessResponse } from '../../utils/responses.util.js';
+import { CloudinaryFolder } from 'src/enums/cloudinaryFolder.enum.js';
+
+type DeleteLogoBrandRequestType = Request<{ public_id: string }, unknown, unknown>;
 
 interface IUploadImageController {
-  logoBrand: (request: Request, response: Response, next: NextFunction) => Promise<void>;
+  uploadLogoBrand: (request: Request, response: Response, next: NextFunction) => Promise<void>;
+  deleteLogoBrand: (request: DeleteLogoBrandRequestType, response: Response, next: NextFunction) => Promise<void>;
 }
 
 export class UploadImageController implements IUploadImageController {
@@ -12,10 +16,10 @@ export class UploadImageController implements IUploadImageController {
 
   constructor() {}
 
-  public async logoBrand(request: Request, response: Response) {
+  public async uploadLogoBrand(request: Request, response: Response): Promise<void> {
     const file = request.file as Express.Multer.File;
 
-    const { publicId, url } = await this._uploadImageService.logoBrand({
+    const { publicId, url } = await this._uploadImageService.uploadLogoBrand({
       fileBuffer: file.buffer,
       originalFileName: file.originalname
     });
@@ -28,6 +32,22 @@ export class UploadImageController implements IUploadImageController {
         data: {
           publicId,
           url
+        }
+      }
+    });
+  }
+
+  public async deleteLogoBrand(request: DeleteLogoBrandRequestType, response: Response): Promise<void> {
+    const { public_id } = request.params;
+    await this._uploadImageService.deleteLogoBrand({ publicId: `${CloudinaryFolder.LOGO_BRAND}/${public_id}` });
+
+    sendSuccessResponse<{ publicId: string }>({
+      response,
+      content: {
+        statusCode: StatusCodes.OK,
+        message: 'Delete logo brand success',
+        data: {
+          publicId: `${CloudinaryFolder.LOGO_BRAND}/${public_id}`
         }
       }
     });
--- src/routes/upload/image.routes.ts
+++ src/routes/upload/image.routes.ts
@@ -1,16 +1,20 @@
 import { Router } from 'express';
 import { UploadImageController } from '../../controllers/upload/image.controller.js';
-import { verifyAccessTokenMiddleware } from '../../middleware/verifyAccessToken.middleware.js';
 import { UploadMulterMiddleware } from '../../middleware/UploadMulter.middleware.js';
+import { verifyAccessTokenMiddleware } from '../../middleware/verifyAccessToken.middleware.js';
 
 export const router = Router();
 
 const uploadImageController = new UploadImageController();
+
+// Middleware for multer upload
 const uploadImageLogoBrandMulterMiddleware = new UploadMulterMiddleware({ fieldName: 'logo' });
 
 router.post(
   '/brand/logo',
   verifyAccessTokenMiddleware,
   uploadImageLogoBrandMulterMiddleware.singleUpload.bind(uploadImageLogoBrandMulterMiddleware),
-  uploadImageController.logoBrand.bind(uploadImageController)
+  uploadImageController.uploadLogoBrand.bind(uploadImageController)
 );
+
+router.delete('/brand/logo/:public_id', uploadImageController.deleteLogoBrand.bind(uploadImageController));
