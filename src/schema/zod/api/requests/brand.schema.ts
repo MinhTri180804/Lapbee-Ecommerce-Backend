5ce56f204550d46cd8d4b7cf7fba5f47761dedbc
Minh Tri <nguyenminhtri1808t@gmail.com>
2025-06-14 18:52:17 +0700

    feat(src): implement api update brand
    Implement an API endpoint to update existing brand information in the system.
    
    Closes #182

--- src/schema/zod/api/requests/brand.schema.ts
+++ src/schema/zod/api/requests/brand.schema.ts
@@ -1,6 +1,13 @@
 import { z } from 'zod';
 import { brandZodSchema } from '../../brand/index.schema.js';
+import { bannersSchema, logoSchema, nameSchema } from '../../brand/field.schema.js';
 
 export const createBrandRequestBodySchema = brandZodSchema;
+export const updateBrandRequestBodySchema = z.object({
+  name: nameSchema.optional(),
+  logo: logoSchema.optional(),
+  banners: bannersSchema.optional()
+});
 
 export type CreateBrandRequestBodySchema = z.infer<typeof createBrandRequestBodySchema>;
+export type UpdateBrandRequestBodySchema = z.infer<typeof updateBrandRequestBodySchema>;
